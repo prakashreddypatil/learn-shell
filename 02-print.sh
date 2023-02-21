3fb8061af5fb61a2307170df2373ca93170f5ab9
patil164 <patilinfo1994@gmail.com>
2023-02-21 20:30:21 +0530

    print helloword d

--- 02-print.sh
+++ 02-print.sh
@@ -1,6 +1,6 @@
 echo helloworld
 
 echo -e "\e[31m message to print \e[0m"
-echo  -e "\e[34m message printed"
-echo -e "\e[35m colour codded disabled \e[0m"
+echo  -e "\e[34m message printed \e[0m"
+echo  colour codded disabled
 
