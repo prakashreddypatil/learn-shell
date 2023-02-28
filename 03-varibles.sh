2cd2145c620a558ee2d9480ae243f63359881e4f
patil164 <patilinfo1994@gmail.com>
2023-02-28 16:23:23 +0530

    varibles date command

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -1,7 +1,12 @@
 DATE=2024-02-28
-echo "Hi hallo, good morning ,the to day date is ${DATE}"
+echo "Hi hallo, good morning ,To day date is ${DATE}"
 
 ##command substitution
 
 DATE=$(date)
 echo "Hi hallo, good morning ,the to day date is ${DATE}"
+
+##arithematic substitution
+ADD=$((2+3))
+echo "the addition of 2+3 =$ADD"
+echo "The addition of 2+3 =${ADD}"
