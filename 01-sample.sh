301e3d3a2066b1c63e6935bf0adfc4b23c0856b3
patil164 <patilinfo1994@gmail.com>
2023-02-28 13:54:23 +0530

    ptinting

---
+++ 01-sample.sh
@@ -0,0 +1,13 @@
+#learn shell
+
+shell scripts Topics
+
+1. printing
+2. Comments
+3. variables
+4. functions
+6. conditions
+7. loops
+8. exit status
+9. Quotes
+10. redirectors
--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,4 @@ echo -e "\e[31m message to print \e[0m"
 echo  -e "\e[34m message printed \e[0m"
 echo  colour codded disabled
 
+echo  -e "\e[36m colour code change  and it stopped here \e[0m"
