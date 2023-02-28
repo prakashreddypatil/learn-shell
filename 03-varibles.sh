debe8c70ef258fdb5ed2360e6ea7e6e42d1bb8ba
patil164 <patilinfo1994@gmail.com>
2023-02-28 21:31:13 +0530

    arrays

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -10,3 +10,11 @@ echo "Hi hallo, good morning ,the to day date is ${DATE}"
 ADD=$((2+3))
 echo "the addition of 2+3 =$ADD"
 echo "The addition of 2+3 =${ADD}"
+
+##array
+Array=(10 20 30)
+echo  "the value of array is ="${Array(0)}
+echo  'the value of array is ='${Array(1)}
+echo  the value of array is = ${Array(3)}
+
+
