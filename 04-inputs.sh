729b9f28aadbf19d1f04730b7986f3d1118901b8
patil164 <patilinfo1994@gmail.com>
2023-02-28 16:48:28 +0530

    giving input

---
+++ 04-inputs.sh
@@ -0,0 +1,11 @@
+# 0-n , $* or $@, $#
+
+echo first argument $1 # first argument
+echo second argument $2 # second argument
+
+echo script name $0 # script name
+
+echo all arguments = $*
+echo all arguments = $@  # All arguments
+
+echo number of arguments = $# #number of arguments
