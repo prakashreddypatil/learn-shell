586173087b1d2b5bdd3488af844cea8aab1c0ce9
patil164 <patilinfo1994@gmail.com>
2023-03-02 23:09:35 +0530

    function

--- 05-function.sh
+++ 05-function.sh
@@ -2,7 +2,14 @@
 
 xyz() {
   echo hello this a function
+  echo the first argumnet $1
+  echo the second argument $2
+  echo the script name $0
+  echo All argumnets $*
+  echo "all arguments $@"
+  echo no of argumnets $#
+
 }
 
 ##calling a function
-xyz
+xyz 123 abc
