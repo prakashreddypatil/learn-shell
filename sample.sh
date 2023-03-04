2ca7890f0e47b01b1eb74e9e92fdb8155a9ecc63
patil164 <patilinfo1994@gmail.com>
2023-03-04 17:15:18 +0530

    mongodb

--- sample.sh
+++ sample.sh
@@ -2,3 +2,33 @@
 #this first bash script to execute in the server
 ls
 ls -all
+Bonbon,Grayson
+Lovebug,Cloud
+Poky,Stormzy
+Zippy	,Steely
+Pip	,Shiny
+Bop,Boby,ball,Wolfie
+Buffie,Smokey
+Itsy,GreyGoose
+Jelly,Bean,Whiskers
+Buggy,Smokey
+Pixel
+Toto
+Pebble
+Speck
+Bumblebee
+Waggy
+Lucky
+Charlie
+Royce
+Buster
+Bolt
+Sonic
+Rusty
+Uno
+Herbie
+Lemon
+Clio
+Astro
+Bambi
+Boo
