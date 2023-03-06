b3d3e166a7cea033609623988938cd34ad6593f8
patil164 <patilinfo1994@gmail.com>
2023-03-06 12:28:01 +0530

    test

---
+++ test123.sh
@@ -0,0 +1,9 @@
+
+## School is an  school institution school  where we gain education and knowledge
+## Most important members of a school are teachers and students.
+## A school follows a fixed curriculum of studies for every class.
+## Students from different cultural and financial backgrounds study under one roof in a school.
+## School prepares the students to face the challenges of the world and life.
+## Apart from studies, a school must also impart skills in its students.
+## A school must have adequate infrastructure and sanitation facilities for the students.
+
