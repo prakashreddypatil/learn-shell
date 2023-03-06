28b83ed9b5033fb67c91620ef651321c1d0996b7
patil164 <patilinfo1994@gmail.com>
2023-03-06 12:31:03 +0530

    test

--- test123.sh
+++ test123.sh
@@ -1,9 +1,12 @@
 
- #School is an  school institution school  where we gain education and knowledge
- #Most important members of a school are teachers and students.
-## A school follows a fixed curriculum of studies for every class.
-## Students from different cultural and financial backgrounds study under one roof in a school.
-## School prepares the students to face the challenges of the world and life.
-## Apart from studies, a school must also impart skills in its students.
-## A school must have adequate infrastructure and sanitation facilities for the students.
-
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "COMPONENT.DOMAIN",
+      "Type": "A",
+      "TTL": 30,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}
