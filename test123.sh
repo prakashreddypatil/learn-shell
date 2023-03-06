ff1e9843757d7b8dfea7199ed8d15b5531d7b5d8
patil164 <patilinfo1994@gmail.com>
2023-03-06 12:29:46 +0530

    test

--- test123.sh
+++ test123.sh
@@ -1,6 +1,6 @@
 
-## School is an  school institution school  where we gain education and knowledge
-## Most important members of a school are teachers and students.
+ #School is an  school institution school  where we gain education and knowledge
+ #Most important members of a school are teachers and students.
 ## A school follows a fixed curriculum of studies for every class.
 ## Students from different cultural and financial backgrounds study under one roof in a school.
 ## School prepares the students to face the challenges of the world and life.
