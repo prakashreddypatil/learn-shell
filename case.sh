6cf8afa047618d602a6b539fa6f11819884c65ba
patil164 <patilinfo1994@gmail.com>
2023-03-07 18:05:42 +0530

    read

--- case.sh
+++ case.sh
@@ -17,3 +17,17 @@ case $name in
     echo "please enter a valid course details"
 esac
 
+
+read -p 'please enter a course name :' name
+
+if [ "${name}" == "DevOps" ];  then
+   echo "the classes start from march 10 2023"
+   echo "the classes timings morning 6am to 8am"
+   echo "the course duration is 1 month"
+elif [ "${name}" == "AWS" ];then
+    echo "the classes start from march 24 2023"
+    echo "the classes timings morning 9am to 10am"
+    echo "the course duration is 2month"
+ else
+    echo "please enter a valid course details"
+    Fi
