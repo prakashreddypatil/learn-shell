c3707bb226ec55efb071142b4eb0eaaa358a6284
patil164 <patilinfo1994@gmail.com>
2023-03-07 16:06:16 +0530

    user

--- 02-print.sh
+++ 02-print.sh
@@ -8,11 +8,12 @@ echo  -e "\e[36m colour code change  and it stopped here \e[0m"
 
 ##to print multiple lines
 echo -e   "line1\nline2\nline3"
+echo -e "\e[34 mline1\nline2\nline3 \e[0m"
 
 ##to print multiple lines
-echo   "\e[32m '
+echo   '
 line 1
 line 1
 line 3
 line 4
-line 5 ' \e[0m"
+line 5 '
