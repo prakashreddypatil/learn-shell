b35eedad7d2a920ee21ae1a1f67ee623b5dfdb18
patil164 <patilinfo1994@gmail.com>
2023-03-07 16:03:13 +0530

    user

--- 02-print.sh
+++ 02-print.sh
@@ -6,11 +6,12 @@ echo  colour codded disabled
 
 echo  -e "\e[36m colour code change  and it stopped here \e[0m"
 
-
+##to print multiple lines
 echo -e   "line1\nline2\nline3"
 
-echo  '
+##to print multiple lines
+echo  '"\e[32m
 line 1
 line 1
 line 3
-line 4 '
+line 4 \e[0m"'
